d656b50f70f5f151cd4d9b61e3c85ba1218a4016
TTTruong <truyen.truong@uoit.net>
2016-03-08 14:25:27 -0500

    Added administrator deposit.

--- Phase3/Administrator.cpp
+++ Phase3/Administrator.cpp
@@ -44,16 +44,6 @@ void Administrator::Withdrawal() {
     return;
   }
 
-  cout << "Enter amount to withdraw: ";
-  cin >> temp_amount;
-
-  if (transactions.is_Amount_Valid(temp_amount)) {
-    amount = stof(temp_amount);
-  } else {
-    cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
-    return;
-  }
-
   if (transactions.Matches(acc_holder, acc_num)) {
 
     if (transactions.is_Disabled(acc_num)) {
@@ -61,6 +51,16 @@ void Administrator::Withdrawal() {
       return;
     } else if (transactions.is_New(acc_num)) {
       cerr << "\n>>> ERROR: Newly created accounts may not withdraw funds. Please try again in 24 hours.\n" << endl;
+      return;
+    }
+
+    cout << "Enter amount to withdraw: ";
+    cin >> temp_amount;
+
+    if (transactions.is_Amount_Valid(temp_amount)) {
+      amount = stof(temp_amount);
+    } else {
+      cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
       return;
     }
 
@@ -87,7 +87,7 @@ void Administrator::Withdrawal() {
           users.at(i).SetBalance(new_balance);
           break;
         } else {
-          cerr << "\n>>> ERROR: The amound entered is invalid.\n" << endl;
+          cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
           return;
         }
       }
@@ -120,7 +120,108 @@ void Administrator::Paybill() {
 }
 
 void Administrator::Deposit() {
-  cout << "Funds have been successfully added to the account " << endl;
+  
+  string padded_acc_holder;
+  string padded_acc_num;
+  string padded_amount;
+  string padded_new_balance;
+  string acc_holder;
+  string temp_acc_num;
+  string temp_amount;
+  stringstream stream;
+  float amount;
+  float new_balance = 0.0;
+  int acc_num;
+
+  cout << "\nDeposit transaction selected.\n" << endl;
+
+  cout << "Enter account holder's name: ";
+  cin >> acc_holder;
+
+  if (!transactions.HolderExists(acc_holder)) {
+    cerr << "\n>>> ERROR: The account holder name entered is not valid.\n" << endl;
+    return;
+  } else if (acc_holder.compare("END_OF_FILE") == 0) {
+    cerr << "\n>>> ERROR: You may not withdraw funds from this account.\n" << endl;
+    return;
+  }
+
+  cout << "Enter account number: ";
+  cin >> temp_acc_num;
+
+  regex re("[0-9]{0,5}");
+  if (regex_match(temp_acc_num, re)) {
+    acc_num = stoi(temp_acc_num);
+  } else {
+    cerr << "\n>>> ERROR: The account number entered is invalid.\n" << endl;
+    return;
+  }
+
+  if (transactions.Matches(acc_holder, acc_num)) {
+
+    if (transactions.is_Disabled(acc_num)) {
+      cerr << "\n>>> ERROR: Disabled accounts may not deposit funds.\n" << endl;
+      return;
+    } else if (transactions.is_New(acc_num)) {
+      cerr << "\n>>> ERROR: Newly created accounts may not deposit funds. Please try again in 24 hours.\n" << endl;
+      return;
+    }
+
+    cout << "Enter amount to deposit: ";
+    cin >> temp_amount;
+
+    if (transactions.is_Amount_Valid(temp_amount)) {
+      amount = stof(temp_amount);
+    } else {
+      cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
+      return;
+    }
+
+    padded_acc_holder = acc_holder;
+    while (padded_acc_holder.length() < 20) {
+      padded_acc_holder = padded_acc_holder + " ";
+    }
+
+    padded_acc_num = temp_acc_num;
+    while (padded_acc_num.length() < 5) {
+      padded_acc_num = "0" + padded_acc_num;
+    }
+
+    padded_amount = temp_amount;
+    while (padded_amount.length() < 8) {
+      padded_amount = "0" + padded_amount;
+    }
+
+    for (int i = 0; i < users.size(); i++) {
+      if (users.at(i).GetNum() == acc_num) {
+
+        if (((amount + users.at(i).GetBalance()) <= 99999.99) && (amount > 0.0)) {
+          new_balance = users.at(i).GetBalance() + amount;
+          users.at(i).SetBalance(new_balance);
+          break;
+        } else {
+          cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
+          return;
+        }
+      }
+    }
+
+    stream << fixed << setprecision(2) << new_balance;
+    padded_new_balance = stream.str();
+    while (padded_new_balance.length() < 8) {
+      padded_new_balance = "0" + padded_new_balance;
+    }
+  
+    cout << "\nFunds have been successfully added to the account " << padded_acc_num << "." << endl;
+    cout << "New balance: $" + padded_new_balance << endl;
+
+    string transaction_line = "01 " + padded_acc_holder + " " + padded_acc_num + " " + padded_amount + "   ";
+    transaction_file.push_back(transaction_line);
+    cout << "\nEnter a command.\n" << endl;
+
+  } else {
+      cerr << "\n>>> ERROR: The account number entered does not match the account holder name.\n" << endl;
+  }
 }
 
 void Administrator::Create() {
