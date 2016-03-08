f4bc5e8a0e995bfe9053503739838e49df8ef1f8
TTTruong <truyen.truong@uoit.net>
2016-03-08 16:09:20 -0500

    Added administrator paybill.

--- Phase3/Administrator.cpp
+++ Phase3/Administrator.cpp
@@ -268,7 +268,117 @@ void Administrator::Transfer() {
 }
 
 void Administrator::Paybill() {
-  cout << "You have successfully paid a bill of $" << endl;
+
+  string padded_acc_holder;
+  string padded_acc_num;
+  string padded_amount;
+  string padded_new_balance;
+  string acc_holder;
+  string temp_acc_num;
+  string temp_amount;
+  string company;
+  stringstream stream;
+  float amount;
+  float new_balance = 0.0;
+  int acc_num;
+
+  cout << "\nPay bill transaction selected.\n" << endl;
+
+  cout << "Enter account holder's name: ";
+  cin >> acc_holder;
+
+  if (!transactions.HolderExists(acc_holder)) {
+    cerr << "\n>>> ERROR: The account holder name entered is not valid.\n" << endl;
+    return;
+  } else if (acc_holder.compare("END_OF_FILE") == 0) {
+    cerr << "\n>>> ERROR: You may not withdraw funds from this account.\n" << endl;
+    return;
+  }
+
+  cout << "Enter account number: ";
+  cin >> temp_acc_num;
+
+  regex re("[0-9]{0,5}");
+  if (regex_match(temp_acc_num, re)) {
+    acc_num = stoi(temp_acc_num);
+  } else {
+    cerr << "\n>>> ERROR: The account number entered is invalid.\n" << endl;
+    return;
+  }
+
+  if (transactions.Matches(acc_holder, acc_num)) {
+
+    if (transactions.is_Disabled(acc_num)) {
+      cerr << "\n>>> ERROR: Disabled accounts may not withdraw funds.\n" << endl;
+      return;
+    } else if (transactions.is_New(acc_num)) {
+      cerr << "\n>>> ERROR: Newly created accounts may not withdraw funds. Please try again in 24 hours.\n" << endl;
+      return;
+    }
+
+    cout << "Enter company: ";
+    cin >> company;
+
+    if (company.compare("EC") != 0 && company.compare("CQ") != 0 && company.compare("TV") != 0) {
+      cerr << "\n>>> ERROR: The company name is invalid. Must be one of the following: EC, CQ or TV.\n" << endl;
+      return;
+    }
+
+    cout << "Enter amount to pay: ";
+    cin >> temp_amount;
+
+    if (transactions.is_Amount_Valid(temp_amount)) {
+      amount = stof(temp_amount);
+    } else {
+      cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
+      return;
+    }
+
+    padded_acc_holder = acc_holder;
+    while (padded_acc_holder.length() < 20) {
+      padded_acc_holder = padded_acc_holder + " ";
+    }
+
+    padded_acc_num = temp_acc_num;
+    while (padded_acc_num.length() < 5) {
+      padded_acc_num = "0" + padded_acc_num;
+    }
+
+    padded_amount = temp_amount;
+    while (padded_amount.length() < 8) {
+      padded_amount = "0" + padded_amount;
+    }
+
+    for (int i = 0; i < users.size(); i++) {
+      if (users.at(i).GetNum() == acc_num) {
+
+        if (amount <= users.at(i).GetBalance() && amount > 0.0) {
+          new_balance = users.at(i).GetBalance() - amount;
+          users.at(i).SetBalance(new_balance);
+          break;
+        } else {
+          cerr << "\n>>> ERROR: The amount entered is invalid.\n" << endl;
+          return;
+        }
+      }
+    }
+
+    stream << fixed << setprecision(2) << new_balance;
+    padded_new_balance = stream.str();
+    while (padded_new_balance.length() < 8) {
+      padded_new_balance = "0" + padded_new_balance;
+    }
+  
+    cout << "\nYou have successfully paid a bill of " << padded_amount << " from account " << padded_acc_num << " to " << company << "." << endl;
+    cout << "New balance: $" + padded_new_balance << endl;
+
+    string transaction_line = "03 " + padded_acc_holder + " " + padded_acc_num + " " + padded_amount + " " + company;
+    transaction_file.push_back(transaction_line);
+    cout << "\nEnter a command.\n" << endl;
+
+  } else {
+      cerr << "\n>>> ERROR: The account number entered does not match the account holder name.\n" << endl;
+  }
 }
 
 void Administrator::Deposit() {
