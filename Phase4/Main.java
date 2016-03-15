65db34f61f490283257e25f399dfa736a1b3092c
TTTruong <truyen.truong@uoit.net>
2016-03-15 16:55:36 -0400

    Added pulling in files to main.

--- Phase4/Main.java
+++ Phase4/Main.java
@@ -13,17 +13,39 @@ public class Main {
 
     if (args.length < 2 || args[1].equals("transaction_file_*.tra")) {
 
-      System.out.println("ERROR: At least 2 arguments are needed: <old_master_accounts> <transaction_files>");
+      System.err.println("ERROR: At least 2 arguments are needed: <old_master_accounts> <transaction_files>");
 
     } else {
 
       oldMasterFileName = args[0];
-      System.out.println("inputMaster: " + oldMasterFileName);
+      //System.out.println("inputMaster: " + oldMasterFileName);
 
       transferFileNames = Arrays.copyOfRange(args, 1, args.length);
 
-      for (int i = 0; i < transferFileNames.length; i++) {
+      /*for (int i = 0; i < transferFileNames.length; i++) {
         System.out.println(transferFileNames[i]);
+      }*/
+
+      // Read in old master bank accounts file.
+      try (BufferedReader br = new BufferedReader(new FileReader(oldMasterFileName))) {
+        String line;
+        while ((line = br.readLine()) != null) {
+          oldMasterAccounts.add(line);
+        }
+      } catch (IOException e) {
+        System.err.println("ERROR: Trouble reading in old master bank accounts file.");
+      }
+
+      // Read in and merge all transaction files.
+      for (int i=0; i<transferFileNames.length; i++) {
+      	try (BufferedReader br = new BufferedReader(new FileReader(transferFileNames[i]))) {
+          String line;
+          while ((line = br.readLine()) != null) {
+            mergedTransferFiles.add(line);
+          }
+        } catch (IOException e) {
+          System.err.println("ERROR: Trouble reading in transaction files.");
+        }
       }
     }
   }
