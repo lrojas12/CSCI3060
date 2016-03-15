083fe46eeebe064760a778ecc84fdd838f64108b
Luisa Rojas <r.luisaf@gmail.com>
2016-03-15 15:54:50 -0400

    Made make files for front and back end

--- Phase4/Main.java
+++ Phase4/Main.java
@@ -3,26 +3,28 @@ import java.util.*;
 
 public class Main {
 	
-	public static String inputMaster;
-	public static String[] transferFiles;
+	public static String oldMasterFileName;
+	public static String[] transferFileNames;
+	public static List<String> merged_transfer_files = new ArrayList<String>();
+	public static List<String> old_master_accounts = new ArrayList<String>();
+	public static List<String> new_master_accounts = new ArrayList<String>();
 
 	public static void main(String[] args) {
 
-		if (args.length < 2 || args[1].equals("transaction_file_*.tra")) {
+		//if (args.length < 2 || args[1].equals("transaction_file_*.tra")) {
 
-			System.out.println("ERROR: At least 2 arguments are needed: <old_master_accounts> <transaction_files>");
+		//	System.out.println("ERROR: At least 2 arguments are needed: <old_master_accounts> <transaction_files>");
 
-		} else {
+		//} else {
 
-			inputMaster = args[0];
-			System.out.println("inputMaster: " + inputMaster);
+			oldMasterFileName = args[0];
+			System.out.println("inputMaster: " + oldMasterFileName);
 
-			transferFiles = Arrays.copyOfRange(args, 1, args.length);
+			transferFileNames = Arrays.copyOfRange(args, 1, args.length);
 
-			for (int i = 0; i < transferFiles.length; i++) {
-				System.out.println(transferFiles[i]);
+			for (int i = 0; i < transferFileNames.length; i++) {
+				System.out.println(transferFileNames[i]);
 			}
-
-		}
+		//}
 	}
 }
