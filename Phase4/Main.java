b987c768486d0c928e7c26a5d77204e0d282bc7d
TTTruong <truyen.truong@uoit.net>
2016-03-15 17:28:32 -0400

    Refactored some code.

--- Phase4/Main.java
+++ Phase4/Main.java
@@ -9,6 +9,46 @@ public class Main {
   public static List<String> oldMasterAccounts = new ArrayList<String>();
   public static List<String> newMasterAccounts = new ArrayList<String>();
 
+  /*
+   * Reads in the old master bank accounts file and stores it.
+   */
+  public static void storeMasterFile() {
+
+  	// Creates a buffered reader for the old master bank accounts file
+  	try (BufferedReader br = new BufferedReader(new FileReader(oldMasterFileName))) {
+      String line;
+      // Reads in the file line by line
+      while ((line = br.readLine()) != null) {
+      	// Stores the line.
+        oldMasterAccounts.add(line);
+      }
+    } catch (IOException e) {
+      System.err.println("ERROR: Trouble reading in old master bank accounts file.");
+    }
+  }
+
+  /*
+   * Reads in, merges and then stores the transaction files.
+   */
+  public static void mergeTransferFiles() {
+
+    // Loops over every transaction file.
+  	for (int i=0; i<transferFileNames.length; i++) {
+
+      // Creates a buffered read for the transaction files.
+      try (BufferedReader br = new BufferedReader(new FileReader(transferFileNames[i]))) {
+        String line;
+        // Reads in the files line by line
+        while ((line = br.readLine()) != null) {
+          // Stores the line.
+          mergedTransferFiles.add(line);
+        }
+      } catch (IOException e) {
+        System.err.println("ERROR: Trouble reading in transaction files.");
+      }
+    }
+  }
+
   public static void main(String[] args) {
 
     if (args.length < 2 || args[1].equals("transaction_file_*.tra")) {
@@ -27,25 +67,20 @@ public class Main {
       }*/
 
       // Read in old master bank accounts file.
-      try (BufferedReader br = new BufferedReader(new FileReader(oldMasterFileName))) {
-        String line;
-        while ((line = br.readLine()) != null) {
-          oldMasterAccounts.add(line);
-        }
-      } catch (IOException e) {
-        System.err.println("ERROR: Trouble reading in old master bank accounts file.");
-      }
+      storeMasterFile();
 
       // Read in and merge all transaction files.
-      for (int i=0; i<transferFileNames.length; i++) {
-      	try (BufferedReader br = new BufferedReader(new FileReader(transferFileNames[i]))) {
-          String line;
-          while ((line = br.readLine()) != null) {
-            mergedTransferFiles.add(line);
-          }
-        } catch (IOException e) {
-          System.err.println("ERROR: Trouble reading in transaction files.");
-        }
+      mergeTransferFiles();
+
+      System.out.println("\n\nMaster Bank Accounts File\n");
+
+      for (int i=0; i<oldMasterAccounts.size(); i++) {
+      	System.out.println(oldMasterAccounts.get(i));
+      }
+
+      System.out.println("\n\nMerged Transfer Files\n");
+      for (int i=0; i<mergedTransferFiles.size(); i++) {
+      	System.out.println(mergedTransferFiles.get(i));
       }
     }
   }
