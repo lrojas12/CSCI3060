ee26113cb23248175b2fb09e930ba6da98ea135c
TTTruong <truyen.truong@uoit.net>
2016-03-30 18:52:38 -0400

    Added User class constructors tests.

--- Phase5/UserTest.java
+++ Phase5/UserTest.java
@@ -16,60 +16,24 @@ import org.junit.Test;
 public class UserTest {
     
     @Test
-    public void test1() {
-    	
-	}
+    public void userConstructorTest1() {
+      User testUser = new User('A');
+
+      assertEquals('A', testUser.getPlan());
+  }
+
+  @Test
+    public void userConstructorTest2() {
+      User testUser = new User("Tarzan", 1, (float)1000.00, 'A', 0, 'N');
+
+      assertEquals("Tarzan", testUser.getName());
+      assertEquals(1, testUser.getNum());
+      assertEquals(1000.00, testUser.getBalance(), 0.02);
+      assertEquals('A', testUser.getStatus());
+      assertEquals('N', testUser.getPlan());
+  }
     
     public static junit.framework.Test suite(){
        return new JUnit4TestAdapter(UserTest.class);
     }
 }
-
-/**
- * STATEMENT COVERAGE
- * 
- * Causes every statement in the program to be executed at least
- * once, giving us confidence that every statement is at least
- * capable of executing correctly
- * 
- * System: Make a test case for each statement in the program,
- * independent of the others.
- *
- * Test must simply cause the statement to be
- * run, ignoring its actions and sub-statements (but still must check that
- * result of test is correct)
- *
- * Completion criterion: A test case for every statement
- */
-
-/**
- * DECISION COVERAGE
- *
- * Causes every decision (if, switch, while, etc.) in the program
- * to be made both ways (or every possible way for switch).
- *
- * System: Design a test case to exercise each decision in the
- * program each way (true / false).
- * 
- * Completion criterion: A test case for each side of each decision.
- */
-
-/**
- * LOOP COVERAGE
- * 
- * This method makes tests to exercise each loop in the program
- * in four different states :
- * - execute body zero times (do not enter loop)
- * - execute body once (i.e., do not repeat)
- * - execute body twice (i.e., repeat once)
- * - execute body many times
- * 
- * Usually used as an enhancement of a statement, block,
- * decision or condition coverage method
- *
- * System: Devise test cases to exercise each loop with zero, one,
- * two and many repetitions
- *
- * Completion criterion: A test for each of these cases for each loop
- */
-
